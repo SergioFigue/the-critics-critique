4933c235475c3835ba4fee1417a565c07506bed5
SergioFigue <sergio.figue.martin@gmail.com>
2021-04-12 16:44:48 +0200

    updater to pipeline

---
+++ scraping/new_links_retriever.py
@@ -0,0 +1,178 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+'''New Links retriever v2 with a break to skip duplicates.
+3 steps: retrieve old and new links, compare and produce new dataframe'''
+
+
+# vandal updater
+def vandal_latest_links(data):
+    all_sites = pd.read_csv(data)
+    filtered_df = all_sites[all_sites['site'] == 'Vandal'].head(30)
+    return filtered_df['url_link'].tolist()
+
+
+def vandal_link_retrieve(pages, data):
+    links = []
+    titles = []
+    old_links = vandal_latest_links(data)
+    repeated = False
+
+    # pages parser
+    for i in range(pages):
+        if repeated:
+            break
+
+        url = f"https://vandal.elespanol.com/analisis/videojuegos/inicio/{i * 45}"
+
+        # building soup
+        html = requests.get(url).content
+        soup = BeautifulSoup(html, 'lxml')
+        articles = soup.find_all('div', {'class': 'caja300 afterclearer'})
+
+        # links & game titles retrieve. New: checking if old to stop adding raws.
+        for a in articles:
+            link = a.find('a')['href']
+
+            if link not in old_links:
+                links.append(link)
+                titles.append(a.find('a')['title'])
+            else:
+                print(f'{len(titles)} new games added.')
+                repeated = True
+                break
+
+    return links, titles
+
+
+# gamereactor updater
+def gamereactor_latest_games(data):
+    all_sites = pd.read_csv(data)
+    filtered_df = all_sites[all_sites['site'] == 'Gamereactor'].head(30)
+    return filtered_df['game'].tolist()
+
+
+def gamereactor_link_retrieve(pages, data):
+    links = []
+    titles = []
+    old_games = gamereactor_latest_games(data)
+    repeated = False
+
+    # pages parser
+    for i in range(pages):
+        if repeated:
+            break
+
+        url = f'https://www.gamereactor.es/analisis/?page={i + 1}'
+
+        # building soup
+        html = requests.get(url).content
+        soup = BeautifulSoup(html, 'lxml')
+        articles = soup.find('section', {'id': 'textlist'}).find_all('article')
+
+        # links & game titles retrieve. New: checking if old to stop adding raws.
+        for article in articles:
+            game = article.find('h3').text
+
+            if game not in old_games:
+                links.append(f"https://www.gamereactor.es{article.find_all('a')[1]['href']}")
+                titles.append(game)
+            else:
+                print(f'{len(titles)} new games added.')
+                repeated = True
+                break
+
+    return links, titles
+
+
+# revogamers updater
+def revogamers_latest_links(data):
+    all_sites = pd.read_csv(data)
+    filtered_df = all_sites[all_sites['site'] == 'revogamers'].head(30)
+    return filtered_df['url_link'].tolist()
+
+
+def revogamers_link_retrieve(pages, data):
+    links = []
+    titles = []
+    old_links = revogamers_latest_links(data)
+    repeated = False
+
+    # pages parser
+    for i in range(pages):
+        if repeated:
+            break
+
+        url = f'https://www.revogamers.net/analisis-w/page/{i + 1}'
+
+        # building soup
+        html = requests.get(url).content
+        soup = BeautifulSoup(html, 'lxml')
+        articles = soup.find_all('h2')
+
+        # links retrieve
+        for a in articles:
+            link = a.find('a')['href']
+
+            if link not in old_links:
+                links.append(link)
+                titles.append(a.find('a')['title'])
+            else:
+                print(f'{len(titles)} new games added.')
+                repeated = True
+                break
+
+    # Deleting non review links
+    for link in links:
+        if 'analisis' not in link:
+            links.remove(link)
+
+    return links, titles
+
+
+# meristation updater
+def meristation_latest_links(data):
+    all_sites = pd.read_csv(data)
+    filtered_df = all_sites[all_sites['site'] == 'meristation'].head(30)
+    return filtered_df['url_link'].tolist()
+
+
+def meristation_link_retrieve(pages, data):
+    links = []
+    old_links = meristation_latest_links(data)
+    repeated = False
+
+    # pages parser
+    for i in range(pages):
+        if repeated:
+            break
+
+        url = f'https://as.com/meristation/analisis/{337 - i}'
+
+        # building soup
+        html = requests.get(url).content
+        soup = BeautifulSoup(html, 'lxml')
+        articles = soup.find_all('h2')
+
+        # links & game titles retrieve. New: checking if old to stop adding raws.
+        for a in articles:
+
+            if a.find('a') == None:
+                pass
+            else:
+                link = a.find('a')['href']
+
+                if link not in old_links:
+                    links.append(link)
+                else:
+                    repeated = True
+                    break
+
+    # Deleting non review links
+    for link in links:
+        if 'analisis' not in link:
+            links.remove(link)
+
+    print(f'{len(links)} new games added.')
+    return links
---
+++ updater.py
@@ -0,0 +1,51 @@
+import argparse
+import pandas as pd
+from sklearn.utils import shuffle
+
+import scraping.Gamereactor as gam
+import scraping.revogamers as rev
+import scraping.meristation as mer
+import scraping.Vandal as van
+import scraping.new_links_retriever as ret
+
+
+def argument_parser():
+    parser = argparse.ArgumentParser(description='Select number of pages')
+    parser.add_argument("-p", "--pages", type=int, dest='pages', required=True,
+                        help="'Enter the number of pages to retrieve per site. "
+                             "Every site export a different number of articles per page")
+    parser.add_argument("-d", "--data", type=str, dest='data', required=True,
+                        help="Rute to the dataframe you want to test, produced with scraping script")
+    args = parser.parse_args()
+    return args
+
+
+def main(pages, data):
+    # import dataframes site by site (3D Juegos ditched scores in Dec 2020)
+    links_gamereactor, titles_gamereactor = ret.gamereactor_link_retrieve(pages, data)
+    gamereactor_df = gam.gamereactor_dataframe(links_gamereactor, titles_gamereactor)
+
+    links_revogamers, titles_revogamers = ret.revogamers_link_retrieve(pages, data)
+    revogamers_df = rev.revogamers_dataframe(links_revogamers, titles_revogamers)
+
+    links_meristation = ret.meristation_link_retrieve(pages, data)
+    meristation_df = mer.meristation_dataframe(links_meristation)
+
+    links_vandal, titles_vandal = ret.vandal_link_retrieve(pages, data)
+    vandal_df = van.vandal_dataframe(links_vandal, titles_vandal)
+
+    # merge, shuffle and export all review together
+
+    old_data = pd.read_csv(data)
+    all_sites = pd.concat([old_data, vandal_df, gamereactor_df, revogamers_df, meristation_df],
+                          ignore_index=True, sort=False)
+    all_sites.dropna(inplace=True)
+    all_sites.reset_index(drop=True, inplace=True)
+    all_sites.to_csv('./data/up_to_date_all.csv', index=False)
+    print(f'Your dataframe is now up to date!')
+
+
+if __name__ == '__main__':
+    arguments = argument_parser()
+    print(arguments.pages)
+    main(arguments.pages, arguments.data)
